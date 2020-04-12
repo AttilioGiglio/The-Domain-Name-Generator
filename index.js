29305a53eb4d0b757438e03d98d9022e0d8934d7
attiliogiglio <attilio.g93@gmail.com>
2020-04-12 16:56:00 -0400

    Add Project Files

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+document.querySelector("#btn").addEventListener("click", () => {
+document.querySelector("#name").innerHTML = domainName();
+});
+
+let pronoun = ['the','this','that'];
+let adj = ['loose','swole','green'];
+let noun = ['giant','dog','goose'];
+    
+
+function domainName (){
+    let i=0;
+    let j=0;
+    let k=0;
+    
+    let temp = "";
+    
+    for (let i=0;i<pronoun.length;i++)
+    {
+        for (let j=0;j<adj.length;j++)
+        {
+            for (let k=0;k<noun.length;k++)
+            {
+                temp += pronoun[i]+adj[j]+noun[k]+".com <br>";
+            }
+        }
+    }
+    return temp;
+}
+
